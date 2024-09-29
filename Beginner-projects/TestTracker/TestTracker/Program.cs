19aca80dad35e9f06f093519f90b5ad5b8e4713f
OfficialLusi <alessiolusal@gmail.com>
2024-09-29 22:12:12 +0200

    First method to write json file

--- Beginner-projects/TestTracker/TestTracker/Program.cs
+++ Beginner-projects/TestTracker/TestTracker/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using TestTracker;
+
+namespace TestTracker;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        try
+        {
+            CreateJsonFile();
+            Console.WriteLine("File JSON creato con successo.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Errore durante la creazione del file JSON: {ex.Message}");
+        }
+    }
+
+    public static void CreateJsonFile()
+    {
+        var exampleTask = new TaskProp(1, "Example task", TaskProp.Status.todo, DateTime.Now, DateTime.Now);
+
+        string jsonString = JsonSerializer.Serialize(exampleTask);
+        File.WriteAllText("exampleTask.json", jsonString);
+    }
+
+}
---
+++ Beginner-projects/TestTracker/TestTracker/TaskProp.cs
@@ -0,0 +1,17 @@
+﻿namespace TestTracker;
+
+public class TaskProp(int id, string description, TaskProp.Status taskStatus, DateTime createdAt, DateTime updatedAt)
+{
+    public int Id { get; set; } = id;
+    public string Description { get; set; } = description;
+    public Status TaskStatus { get; set; } = taskStatus;
+    public DateTime CreatedAt { get; set; } = createdAt;
+    public DateTime UpdatedAt { get; set; } = updatedAt;
+
+    public enum Status
+    {
+        todo,
+        inProgress,
+        done
+    }
+}
