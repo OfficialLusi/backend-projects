4a6e7d2ec4d9865266003f678050093e219fb362
Alessio Lusa <A.lusa@danieli.it>
2024-11-20 15:49:40 +0100

    project terminated

--- Beginner-projects/UnitConverter/Controllers/HomeController.cs
+++ Beginner-projects/UnitConverter/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-using UnitConverter.Models;
 
 namespace UnitConverter.Controllers
 {
@@ -18,15 +16,116 @@ namespace UnitConverter.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Length()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Length(double value, string unitFrom, string unitTo)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            double result = ConvertLength(value, unitFrom, unitTo);
+            ViewData["Result"] = $"{value} {unitFrom} are {result} {unitTo}";
+            return View();
         }
+
+        private double ConvertLength(double value, string unitFrom, string unitTo)
+        {
+            if (unitFrom == unitTo)
+                return value;
+
+            double valueinMeters = unitFrom switch
+            {
+                "meters" => value,
+                "kilometers" => value * 1000,
+                "miles" => value * 1609.34,
+                _ => throw new NotImplementedException()
+            };
+
+            return unitTo switch
+            {
+                "meters" => valueinMeters,
+                "kilometers" => valueinMeters / 1000,
+                "miles" => valueinMeters / 1609.34,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        public IActionResult Weight()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Weight(double value, string unitFrom, string unitTo)
+        {
+            double result = ConvertWeight(value, unitFrom, unitTo);
+            ViewData["Result"] = $"{value} {unitFrom} are {result} {unitTo}";
+            return View();
+        }
+
+        private double ConvertWeight(double value, string unitFrom, string unitTo)
+        {
+            if (unitFrom == unitTo)
+                return value;
+
+            double valueinGrams = unitFrom switch
+            {
+                "grams" => value,
+                "kilograms" => value * 1000,
+                "tons" => value * 1000000,
+                _ => throw new NotImplementedException()
+            };
+
+            return unitTo switch
+            {
+                "grams" => valueinGrams,
+                "kilograms" => valueinGrams / 1000,
+                "tons" => valueinGrams / 1000000,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        public IActionResult Temperature()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Temperature(double value, string unitFrom, string unitTo)
+        {
+            double result = ConvertTemperature(value, unitFrom, unitTo);
+            ViewData["Result"] = $"{value} {unitFrom} are {result} {unitTo}";
+            return View();
+        }
+
+        private double ConvertTemperature(double value, string unitFrom, string unitTo)
+        {
+            if (unitFrom == unitTo)
+                return value;
+
+            double valueinCelsius = unitFrom switch
+            {
+                "celsius" => value,
+                "farenheit" => (value * (5/9)) - 32,
+                "kelvin" => value - 273.15,
+                _ => throw new NotImplementedException()
+            };
+
+            return unitTo switch
+            {
+                "celsius" => valueinCelsius,
+                "farenheit" => (valueinCelsius * (9 / 5)) + 32,
+                "kelvin" => valueinCelsius + 273.15,
+                _ => throw new NotImplementedException()
+            };
+        }
+
+
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
